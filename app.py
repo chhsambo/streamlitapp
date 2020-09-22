89f4634cba79222a033543193fcb4e0236aecbef
Sambo Chhay <chhaysambo@gmail.com>
2020-09-22 15:51:13 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import streamlit as st
+
+
+@st.cache
+def get_data():
+    url = "http://data.insideairbnb.com/united-states/ny/new-york-city/2019-09-12/visualisations/listings.csv"
+    return pd.read_csv(url)
+
+df = get_data()
+st.line_chart(df)
